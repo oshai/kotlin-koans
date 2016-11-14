23ecfd95edb18249326db857f1a33b10d42ad446
oshai <ohadshai@gmail.com>
2016-11-14 23:39:40 +0200

    more koans

--- src/i_introduction/_7_Nullable_Types/NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/NullableTypes.kt
@@ -22,10 +22,16 @@ fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO
     references = { JavaCode7().sendMessageToClient(client, message, mailer) }
 )
 
-fun sendMessageToClient(
-        client: Client?, message: String?, mailer: Mailer
-) {
-    todoTask7(client, message, mailer)
+fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
+    //if (message == null) return
+
+    message?.run {
+        val personalInfo = client?.personalInfo ?: return
+
+        val email = personalInfo.email ?: return
+
+        mailer.sendMessage(email, message)
+    }
 }
 
 class Client (val personalInfo: PersonalInfo?)
